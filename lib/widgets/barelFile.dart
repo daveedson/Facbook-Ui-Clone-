62870c93fc25b8f183118c7c174c27b5955dab2d
David Onoh <daveeddrumz@gmail.com>
2021-06-24 12:33:07 +0100

    Created the list to hold all the posts

--- lib/widgets/barelFile.dart
+++ lib/widgets/barelFile.dart
@@ -1,3 +1,4 @@
 export 'createPostContainer.dart';
 export 'Rooms.dart';
 export 'stories.dart';
+export 'postContainer.dart';
--- lib/widgets/postContainer.dart
+++ lib/widgets/postContainer.dart
@@ -1,8 +1,83 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_responsive_ui/models/post_model.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/profileAvatar.dart';
 
 class PostContainer extends StatelessWidget {
+  final Post post;
+
+  const PostContainer({Key key, this.post}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.white,
+      margin: const EdgeInsets.symmetric(vertical: 5.0),
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            _PostHeader(post: post),
+           SizedBox(height: 4.0),
+           Text(post.caption),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _PostHeader extends StatelessWidget {
+  final Post post;
+
+  const _PostHeader({
+    Key key,
+    @required this.post,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        ProfileAvatar(imageUrl: post.user.imageUrl),
+        SizedBox(width: 8.0),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                post.user.name,
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Row(
+                children: [
+                  Text(
+                    '${post.timeAgo}',
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                      fontSize: 12.0,
+                    ),
+                  ),
+                  Icon(
+                    Icons.public,
+                    size: 12.0,
+                    color: Colors.grey[600],
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+        IconButton(
+          icon: Icon(Icons.more_horiz_outlined),
+          onPressed: () {},
+        )
+      ],
+    );
   }
 }
